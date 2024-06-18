a61cf15aea5c3b69185bdc02aa4a07c55e3e0856
Thomas Elvin <thomas.e.s.l@outlook.dk>
2024-06-18 23:56:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>PoseidoNET</title>
 <link rel="stylesheet" href="sys/css/styles.css"> <!-- Link to the external CSS file -->
-<link id="theme-color" rel="stylesheet" href="sys/css/default-crt.css">
+<link id="theme-color" rel="stylesheet" href="sys/css/green-crt.css">
 </head>
 <body>
   <div id="terminal-wrapper">
